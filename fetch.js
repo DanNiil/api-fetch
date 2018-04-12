3bac3777846cce3ce7be89ae20a633a7aa36bf43
Daniel N <nilssond1@gmail.com>
2018-04-12 15:34:41 +0200

    Initial fetch.js

---
+++ fetch.js
@@ -0,0 +1,10 @@
+
+/** API Data Fetcher
+ *  Generalized JSON Data Fetcher
+ *  Params entered in config.js
+ */
+
+const config = require('./config');
+const parse = require('./parse');
+
+console.log(config.public);
